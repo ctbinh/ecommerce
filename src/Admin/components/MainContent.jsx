30669e58dd847bd02ba8a8031db2d0e1a6736c41
hoangkui <hoangkui2001@gmail.com>
2022-03-22 10:36:58 +0700

    chart

--- src/Admin/components/MainContent.jsx
+++ src/Admin/components/MainContent.jsx
@@ -204,14 +204,6 @@ const MainContent = () => {
               <CustomerCountry>Vietnam</CustomerCountry>
             </CustomerRight>
           </CustomerItem>
-
-          <CustomerItem>
-            <CustomerImg src="https://scontent.fsgn5-6.fna.fbcdn.net/v/t1.6435-9/148352211_1315852945481787_1410223456476714730_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=shoA0N3QZLUAX_ZqFow&_nc_ht=scontent.fsgn5-6.fna&oh=00_AT-E3sNuRhQXmASthK8E3eCWN_zvMiVxhfLyp68OPCvYgg&oe=6258EF21"></CustomerImg>
-            <CustomerRight>
-              <CustomerName>Tran Hoang</CustomerName>
-              <CustomerCountry>Vietnam</CustomerCountry>
-            </CustomerRight>
-          </CustomerItem>
         </CustomerList>
       </Custommers>
     </MainContentWrap>
